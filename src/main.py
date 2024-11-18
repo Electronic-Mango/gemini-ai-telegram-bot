2087ff09ad322db2ac74f737584640d27241ec64
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-11-18 23:08:25 +0100

    Migrate to Telethon

--- src/main.py
+++ src/main.py
@@ -1,76 +1,59 @@
 from io import BytesIO
-from logging import INFO, basicConfig
-from mimetypes import guess_type
 from os import getenv
-from textwrap import wrap
-from typing import Sequence
 
 from dotenv import load_dotenv
-from telegram import Message, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
-from telegram.ext.filters import COMMAND
+from telethon import TelegramClient
+from telethon.events import NewMessage, StopPropagation
+from telethon.tl.patched import Message
 
 from chat import initial_message, next_message, reset_conversation
-from user_filer import user_filter
 
-(INPUT_PROMPT_STATE,) = range(1)
+load_dotenv()
+SESSION = "Gemini AI"
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+ALLOWED_USERS = getenv("ALLOWED_USERNAMES", "").split()
+
+bot = TelegramClient(SESSION, API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 
 def main() -> None:
-    load_dotenv()
-    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO)
-    bot = ApplicationBuilder().token(getenv("BOT_TOKEN")).build()
-    bot.add_handler(CommandHandler("start", start, user_filter))
-    bot.add_handler(CommandHandler("restart", restart, user_filter))
-    bot.add_handler(MessageHandler(user_filter & ~COMMAND, talk))
-    bot.run_polling()
+    bot.run_until_disconnected()
 
 
-async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    message = await initial_message()
-    await send(message, update)
+@bot.on(NewMessage(from_users=ALLOWED_USERS, incoming=True, pattern="/start"))
+async def start(event: NewMessage.Event) -> None:
+    async with bot.action(event.chat_id, "typing"):
+        message = await initial_message()
+        await event.respond(message)
+    raise StopPropagation
 
 
-async def restart(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    reset_conversation(update.effective_chat.id)
-    await send("Conversation restarted.", update)
+@bot.on(NewMessage(from_users=ALLOWED_USERS, incoming=True, pattern="/restart"))
+async def restart(event: NewMessage.Event) -> None:
+    async with bot.action(event.chat_id, "typing"):
+        reset_conversation(event.chat_id)
+        await event.respond("Conversation restarted.")
+    raise StopPropagation
 
 
-async def talk(update: Update, ctx: ContextTypes.DEFAULT_TYPE) -> None:
-    message = update.effective_message
-    text = message.text or message.caption or ""
-    file_data = await get_file(ctx, message)
-    response = await next_message(update.effective_chat.id, text, file_data)
-    await send(response, update)
+@bot.on(NewMessage(from_users=ALLOWED_USERS, incoming=True))
+async def talk(event: NewMessage.Event) -> None:
+    async with bot.action(event.chat_id, "typing"):
+        message = event.message
+        prompt = message.text
+        files = await get_file(message)
+        response = await next_message(event.chat_id, prompt, files)
+        await event.respond(response)
 
 
-async def get_file(ctx: ContextTypes.DEFAULT_TYPE, message: Message) -> list[tuple[BytesIO, str]]:
-    if not (attachment := message.effective_attachment):
+async def get_file(message: Message) -> list[tuple[BytesIO, str]]:
+    if not (file := message.file):
         return []
-    if isinstance(attachment, Sequence):
-        attachment = max(attachment, key=lambda p: p.file_size or 0)
-    file = await ctx.bot.get_file(attachment)
-    file_array = await file.download_as_bytearray()
-    mime_type = getattr(attachment, "mime_type", None) or guess_type(file.file_path)[0]
-    return [(BytesIO(file_array), mime_type)]
-
-
-async def send(message: str, update: Update) -> None:
-    max_length = int(getenv("MAX_MESSAGE_LENGTH", 4096))
-    if len(message) <= max_length:
-        await update.message.reply_text(message.strip())
-        return
-    parts = wrap(
-        message,
-        max_length,
-        tabsize=4,
-        break_long_words=False,
-        replace_whitespace=False,
-        break_on_hyphens=False,
-        drop_whitespace=False,
-    )
-    for partial in parts:
-        await update.message.reply_text(partial.strip())
+    media = await message.download_media(file=bytes)
+    mime_type = file.mime_type
+    return [(BytesIO(media), mime_type)]
 
 
 if __name__ == "__main__":
